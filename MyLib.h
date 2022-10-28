ed77832a5cf85175d128b60f78e38b2c342ae218
Ariel de la Cruz <90809998+RELmon25@users.noreply.github.com>
2022-10-28 15:09:03 -0500

    Update MyLib.h

--- MyLib.h
+++ MyLib.h
@@ -1,10 +1,13 @@
 #include <string>
-#include "MyMap.h"
 
 using namespace std;
 
 void setCollection(string s1);
 void createProfile(string s2);
 void showCollection();
-void play();
 pair<int,string> randomItem();
+void ganaste();
+void preparar();
+string itemtostring(int);
+void relations();
+void itemManagement();
