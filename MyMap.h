8ebc873ed3c281c54d5e728839f1a18d40d3400b
Ariel de la Cruz <90809998+RELmon25@users.noreply.github.com>
2022-10-29 10:49:42 -0500

    Update MyMap.h

--- MyMap.h
+++ MyMap.h
@@ -17,6 +17,83 @@ class MyMap{
         int maximo;
         int ubicacion;
         int item;
+/*
+        MyMap();
+        MyMap(string _name);
+        void setName(string s);
+        void ShowMap();
+        const MyMap& operator=(const MyMap &other);
+        void menu();
+        void addSub(string);
+        void addSup(string);
+};
+
+MyMap::MyMap(){ name = "default"; item = -1; maximo = 0; ubicacion = 0; }
+
+MyMap::MyMap(string _name){
+    name = _name;
+    item = -1;
+    maximo = 0;
+    ubicacion = 0;
+}
+
+void MyMap::setName(string s){
+    name = s;
+}
+
+const MyMap& MyMap::operator=(const MyMap &other){
+    this->name = other.name;
+    this->submaps = other.submaps;
+    this->supermaps = other.submaps;
+    this->maximo = other.maximo;
+    this->ubicacion = other.ubicacion;
+    this->item = other.item;
+    return *this; 
+}
+
+
+void MyMap::ShowMap(){
+    try{
+        ifstream file;
+        string text;
+        file.open(name+".txt",ios::in);
+        while(!file.eof()){
+            getline(file, text);
+            if(text == "\\n") cout<<"\n";
+            else{
+                if(isdigit(text[0])) cout<<"\e["<<text<<"m";
+                else cout<<text;
+            }
+        }
+        file.close();
+        return;
+    }
+    catch(MyException &e){
+        cout<<e.what()<<endl;
+    }
+}
+
+void MyMap::menu(){
+    cout<<"\n\n\e[34m\t\t\t\t\t- Menu -\n";
+    for(int i=1;i<=maximo;i++){
+        cout<<"\t\t\t\t("<<i<<") \n";
+    }
+    for(int i=0;i<submaps.size();i++){
+        cout<<"\t\t\t\t("<<i+1<<") "<<submaps[i]<<"\n";
+    }
+    for(int i=0;i<supermaps.size();i++){
+        cout<<"\t\t\t\t("<<i+1+submaps.size()<<") "<<supermaps[i]<<"\n";
+    }
+    cout<<"\t\t\t\t("<<submaps.size()+supermaps.size()+maximo+1<<") Menu Inicial\n";
+}
+
+void MyMap::addSub(string s){
+    this->submaps.push_back(s);
+}
+void MyMap::addSup(string s){
+    this->supermaps.push_back(s);
+}
+*/
     MyMap(){ name = "default"; item = -1; maximo = 0; ubicacion = 0; }
 
     MyMap(string _name){
@@ -68,10 +145,10 @@ class MyMap{
             cout<<"\t\t\t\t("<<i<<") \n";
         }
         for(int i=0;i<submaps.size();i++){
-            cout<<"\t\t\t\t("<<i+1<<") "<<submaps[i]<<"\n";
+            cout<<"\t\t\t\t("<<i+1+maximo<<") "<<submaps[i]<<"\n";
         }
         for(int i=0;i<supermaps.size();i++){
-            cout<<"\t\t\t\t("<<i+1+submaps.size()<<") "<<supermaps[i]<<"\n";
+            cout<<"\t\t\t\t("<<i+1+submaps.size()+maximo<<") "<<supermaps[i]<<"\n";
         }
         cout<<"\t\t\t\t("<<submaps.size()+supermaps.size()+maximo+1<<") Menu Inicial\n";
     }
